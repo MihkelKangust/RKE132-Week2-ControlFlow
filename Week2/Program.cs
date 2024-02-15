666abaf9e0abbd626a5ca20074b7e00dee45c934
Mihkel Kangust <mihkel.kangust@gmail.com>
2024-02-15 15:41:56 +0200

    add week2 code

---
+++ Week2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// uued andmetüübid int ja char
+//if-statements (control flow)
+using System.Net.Security;
+
+Console.WriteLine("Hello, World!");
